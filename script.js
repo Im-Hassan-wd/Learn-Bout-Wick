6c2094720dd44483da9fdd0c125192f16399c046
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-30 21:15:16 +0100

    adding event

---
+++ script.js
@@ -0,0 +1,14 @@
+const bird = document.querySelector(".bird");
+
+window.addEventListener("keydown", (e) => {
+    moveUp(e);
+});
+
+function moveUp(e) {
+    const key = e.key;
+    console.log(e)
+
+    if(key == '') {
+
+    }
+}
