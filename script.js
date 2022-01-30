a38bab139f69cf35a20fa17cd5ce6bd49dee3afa
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-30 21:26:14 +0100

    what am i doing.....

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const bird = document.querySelector(".bird");
+const button = document.querySelector("button");
+const gameArea = document.querySelector(".game-area");
 
+//event listerners
 window.addEventListener("keydown", (e) => {
     moveUp(e);
 });
+button.addEventListener("click", expandWorld);
 
+//function
 function moveUp(e) {
     const key = e.key;
 
@@ -13,4 +18,8 @@ function moveUp(e) {
     else if(key == 'ArrowDown') {
         console.log("you pressed" + key)
     }
+}
+
+function expandWorld() {
+    
 }
