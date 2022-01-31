7c867c78fa1877d71a56006e9c5dca13bb9242c1
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-01-31 14:32:40 +0100

    making movement possible

--- script.js
+++ script.js
@@ -2,23 +2,31 @@ const bird = document.querySelector(".bird");
 console.log(bird)
 const button = document.querySelector("button");
 let gameArea = document.querySelector(".game-area");
+let distance = 10;
 
 //event listerners
-window.addEventListener("keydown", (e) => {
-    moveUp(e);
-});
+window.addEventListener("keypress", (e) => moveUp(e));
+window.addEventListener("keyup", (e) => moveDown(e));
 button.addEventListener("click", expandWorld);
 
 //function
+function moveDown(e) {
+    const key = e.key;
+
+    if(key == ' ') {
+        bird.style.transform = "translateY(100px)";
+    }
+    else if(key == 'ArrowDown') {
+        distance += 5; 
+        
+    }
+}
+
 function moveUp(e) {
     const key = e.key;
 
-    if(key == 'ArrowUp') {
-        bird.style.transform = "translateY(0px)";
-    }
-    else if(key == 'ArrowDown') {
-        console.log("you pressed" + key)
-        bird.style.transform = "translateY(30px)";
+    if (key == ' ') {
+        bird.style.transform = "translateY(50px)";
     }
 }
 
@@ -27,5 +35,5 @@ function expandWorld() {
     setInterval(() => {
         window.outerWidth += 100;
         gameArea.style.width = window.outerWidth + "px";
-    }, 1000);
+    }, 5000);
 }
