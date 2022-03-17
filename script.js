f083c355e3f1fd2511ba10133fa1462347239f07
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-17 15:50:02 +0100

    review overview functtion

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const reviewOverview = document.querySelector('.review-overview');
-
+const path = [
+    './asset/8.jpg', './asset/10.jpg', './asset/6.jpg'
+]
 // functions
 function createOverview (){
+  let index = Math.floor(Math.random() * path.length);
   // oveview image div
   const imageDiv = document.createElement('div');
+  reviewOverview.append(imageDiv);
 
   // overiew image
+  const image = document.createElement('img');
+  image.src = path[index];
+  image.alt = 'avatar';
+  imageDiv.append(image);
 }
+
+setInterval(() => {
+//  createOverview();
+},3000)
