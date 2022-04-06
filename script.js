1adc599ead65a46c662da414aa5be88e45ad93e0
Hassan Warwick <82989580+Im-Hassan-wd@users.noreply.github.com>
2022-04-06 23:45:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const body = document.querySelector('body');
-const projectDiv = document.querySelectorAll('.project div');
+const projectDivs = document.querySelectorAll('.project div');
 
 // Event listeners
 body.addEventlistener('contextmenu', (e) => e.preventDefault() );
@@ -7,11 +7,13 @@ window.addEventListener('scroll', scrollAppear)
 
 // Functions 
 function scrollAppear(){
-  const projectDivPosition = projectDiv.getBoundingClientRect().top;
-  const screenPosition = window.imnerHeight / 1.3;
-  if (projectDiv < screenPosition) {
-    projectDiv.classList.add('active');
-  }
+  projectDivs.foreach(div => {
+    const projectDivPosition = div.getBoundingClientRect().top;
+    const screenPosition = window.imnerHeight / 1.3;
+    if (projectDivPosition < screenPosition) {
+      projectDiv.classList.add('active');
+    }
+  });
 }
 
 
