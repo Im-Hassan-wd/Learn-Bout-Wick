4be8b443478ab322d37da38b2a78a302ad311005
Im-Hassan-wd <alhassansahih@gmail.com>
2022-04-07 12:35:34 +0100

    javascript

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const body = document.querySelector('body');
 const projectDivs = document.querySelectorAll('.project div');
 
 // Event listeners
-body.addEventlistener('contextmenu', (e) => e.preventDefault() );
-window.addEventListener('scroll', scrollAppear)
+window.addEventListener('scroll', scrollAppear);
 
 // Functions 
 function scrollAppear(){
-  projectDivs.foreach(div => {
+  projectDivs.forEach(div => {
     const projectDivPosition = div.getBoundingClientRect().top;
     const screenPosition = window.innerHeight / 1.3;
     if (projectDivPosition < screenPosition) {
