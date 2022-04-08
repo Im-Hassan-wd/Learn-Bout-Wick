3be5bd849f90605d256988d5323ba37cb47fcf03
Im-Hassan-wd <alhassansahih@gmail.com>
2022-04-08 17:24:45 +0100

    context menu

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const body = document.querySelector('body');
 const projectDivs = document.querySelectorAll('.project div');
+const allImages = document.querySelectorAll('img');
 
 // Event listeners
 window.addEventListener('scroll', scrollAppear);
+allImages.forEach((image) => {
+  image.addEventListener('contextmenu', e => {
+    e.preventDefault();
+  })
+})
 
 // Functions 
 function scrollAppear(){
@@ -11,8 +17,6 @@ function scrollAppear(){
     const screenPosition = window.innerHeight / 2;
     if (projectDivPosition < screenPosition) {
       div.classList.add('appear');
-    } else {
-      div.classList.remove('appear');
     }
   });
 }
