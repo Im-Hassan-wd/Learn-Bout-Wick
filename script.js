dd370756187ff989f01f720d1a4f2feb9a0a8017
Im-Hassan-wd <alhassansahih@gmail.com>
2022-04-11 18:37:36 +0100

    nav classlist

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const body = document.querySelector('body');
 const projectDivs = document.querySelectorAll('.project div');
 const projectImages = document.querySelectorAll('.project img');
 const allImages = document.querySelectorAll('img');
+const navLists = document.querySelectorAll('nav li a');
 
 // Event listeners
 window.addEventListener('scroll', () => {
@@ -13,7 +14,14 @@ allImages.forEach((image) => {
   image.addEventListener('contextmenu', e => {
     e.preventDefault();
   })
-})
+});
+
+navLists.forEach(list => {
+  list.addEventListener('click', (e) => {
+    list.classList.remove('active');
+    addActive(e)
+  })
+});
 
 // Functions 
 function scrollAppear(contents){
@@ -24,6 +32,10 @@ function scrollAppear(contents){
       content.classList.add('appear');
     }
   });
+}
+
+function addActive (e) {
+  e.target.classList.add('active');
 }
 
 
