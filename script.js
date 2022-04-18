1390449506361a32645226153910bb570ec3761f
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-18 19:21:58 +0100

    card animation

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const body = document.querySelector('body');
 const projectDivs = document.querySelectorAll('.project div');
 const projectImages = document.querySelectorAll('.project img');
 const navLists = document.querySelectorAll('nav li a');
+const cards = document.querySelectorAll('.card');
 
 // Event listeners
 window.addEventListener('scroll', () => {
@@ -16,6 +17,11 @@ navLists.forEach(list => {
   })
 });
 
+cards.forEach(card => {
+  card.addEventListener('mouseenter', (e) => cardAnimation(e));
+  card.addEventListener('mouseleave', (e) => removeAnimation(e));
+})
+
 // Functions 
 function scrollAppear(contents){
   contents.forEach(content => {
@@ -29,6 +35,17 @@ function scrollAppear(contents){
 
 function addActive (e) {
   e.target.classList.add('active');
+}
+
+function cardAnimation (e){
+  let animate = e.target.children[2];
+  animate.style.transition = '0.5s ease';
+  animate.style.transform = 'translateY(-5px)';
+}
+
+function removeAnimation (e){
+  let animate = e.target.children[2];
+  animate.style.transform = 'translateY(0px)';
 }
 
 
