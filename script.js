c535a34400a1f7cfa86c3cbc46114f66493c807e
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-18 04:32:22 +0100

    projects

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 const body = document.querySelector('body');
 const projectDivs = document.querySelectorAll('.project div');
 const projectImages = document.querySelectorAll('.project img');
-const allImages = document.querySelectorAll('img');
 const navLists = document.querySelectorAll('nav li a');
 
 // Event listeners
 window.addEventListener('scroll', () => {
   scrollAppear(projectDivs);
   scrollAppear(projectImages);
-});
-
-allImages.forEach((image) => {
-  image.addEventListener('contextmenu', e => {
-    e.preventDefault();
-  })
 });
 
 navLists.forEach(list => {
